9103efc7e43ca6fc52dbcf11236cece5ac46b56e
Raymond <zhrangui@hotmail.com>
2019-05-15 22:56:59 -0400

    Comment

--- Elixir/lab2/lab2.py
+++ Elixir/lab2/lab2.py
@@ -1,5 +1,5 @@
-immutable_array = (1, 2, 3, 4, 5, 6)
-def square(immutable):
-    result = map(lambda x: pow(x, 2), immutable)
+immutable_array = (1, 2, 3, 4, 5, 6) #Immutability
+def square(immutable): #Pure Functions
+    result = map(lambda x: pow(x, 2), immutable) ##Pure Functions
     return result
 print(list(square(immutable_array)))
