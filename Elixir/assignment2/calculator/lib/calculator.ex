aafa4d07be5160e30562d4b419f72d962ceef38d
Raymond <zhrangui@hotmail.com>
2019-05-27 20:49:43 -0400

    Init calculator project

---
+++ Elixir/assignment2/calculator/lib/calculator.ex
@@ -0,0 +1,18 @@
+defmodule Calculator do
+  @moduledoc """
+  Documentation for Calculator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Calculator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
