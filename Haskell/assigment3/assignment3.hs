469d645c532e8648bfc9341dd3fb99107fb3f451
rui zhang <zhrangui@outlook.com>
2019-06-24 09:05:24 -0400

    Add unitest

---
+++ Haskell/assigment3/assignment3.hs
@@ -0,0 +1,28 @@
+module Assignment3 where
+    import Test.QuickCheck
+    import Control.Monad ( forM, forM_, liftM )
+    import Debug.Trace ( trace )
+    import System.Directory ( doesDirectoryExist, getDirectoryContents )
+    import System.Environment ( getArgs )
+    import System.FilePath ( (</>) )
+    
+    getRecursiveContents :: FilePath -> IO [FilePath]
+    getRecursiveContents topPath = do
+      names <- getDirectoryContents topPath
+      let
+        properNames =
+          filter (`notElem` [".", ".."]) $
+          trace ("Processing " ++ topPath) names
+      paths <- forM properNames $ \name -> do
+        let path = topPath </> name
+        isDirectory <- doesDirectoryExist path
+        if isDirectory
+          then getRecursiveContents path
+          else return [path]
+      return (concat paths)
+
+
+    simpleFind :: (FilePath -> Bool) -> FilePath -> IO [FilePath]
+    simpleFind p path = do
+        names <- getRecursiveContents path
+        return (filter p names)
---
+++ Haskell/assigment3/tests.hs
@@ -0,0 +1,4 @@
+import Test.HUnit
+import Assignment3
+
+test1 = TestCase (assertEqual "for (foo 3)," (simpleFind "simpleFind") "simpleFind")
