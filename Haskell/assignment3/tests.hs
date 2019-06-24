407bc21cb40e77a1f12635442b46a4f113babb3e
rui zhang <zhrangui@outlook.com>
2019-06-24 18:44:00 -0400

    clean code

--- Haskell/assignment3/tests.hs
+++
@@ -1,7 +0,0 @@
-module Assignment3Test where
-    import Test.HUnit
-    import Assignment3
-
-    test1 = TestCase (assertEqual "for (foo 3)," "./assignment.hs" (foo "./assignment1.hs"))
-    --test1 = TestCase (assertEqual "for (foo 3)," (1,2) (foo 3))
-    tests = TestList [TestLabel "test1" test1]
