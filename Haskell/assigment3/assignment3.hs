d7019fe8af680ba5b1e98205982041ba0819b777
raymond zhang <raymond.zhang@bellmedia.ca>
2019-06-24 11:19:40 -0400

    Another file search

--- Haskell/assigment3/assignment3.hs
+++ Haskell/assigment3/assignment3.hs
@@ -1,28 +1,46 @@
 module Assignment3 where
-    import Test.QuickCheck
-    import Control.Monad ( forM, forM_, liftM )
-    import Debug.Trace ( trace )
-    import System.Directory ( doesDirectoryExist, getDirectoryContents )
-    import System.Environment ( getArgs )
-    import System.FilePath ( (</>) )
-    
-    getRecursiveContents :: FilePath -> IO [FilePath]
-    getRecursiveContents topPath = do
-      names <- getDirectoryContents topPath
-      let
-        properNames =
-          filter (`notElem` [".", ".."]) $
-          trace ("Processing " ++ topPath) names
-      paths <- forM properNames $ \name -> do
-        let path = topPath </> name
-        isDirectory <- doesDirectoryExist path
-        if isDirectory
-          then getRecursiveContents path
-          else return [path]
-      return (concat paths)
+  import Control.Monad ( forM, forM_, liftM )
+  import Debug.Trace ( trace )
+  import System.Directory
+  import System.FilePath.Posix
+  import System.IO
+
+  getRecursiveContents :: FilePath -> IO [FilePath]
+  getRecursiveContents topPath = do
+    names <- getDirectoryContents topPath
+    let
+      properNames =
+        filter (`notElem` [".", ".."]) $
+        trace ("Processing " ++ topPath) names
+    paths <- forM properNames $ \name -> do
+      let path = topPath </> name
+      isDirectory <- doesDirectoryExist path
+      if isDirectory
+      then getRecursiveContents path
+      else return [path]
+    return (concat paths)
 
 
-    simpleFind :: (FilePath -> Bool) -> FilePath -> IO [FilePath]
-    simpleFind p path = do
-        names <- getRecursiveContents path
-        return (filter p names)
+  simpleFind :: (FilePath -> Bool) -> FilePath -> IO [FilePath]
+  simpleFind p path = do
+    names <- getRecursiveContents path
+    return (filter p names)
+   
+  dir_walk :: FilePath -> (FilePath -> IO ()) -> IO ()
+  dir_walk top filefunc = do
+    isDirectory <- doesDirectoryExist top
+    if isDirectory
+      then 
+        do
+          files <- listDirectory top
+          mapM_ (\file -> dir_walk (top </> file) filefunc) files
+      else
+        filefunc top
+   
+  main :: IO ()
+  main = do
+    let worker fname = do
+        if (takeExtension fname == ".hs")
+          then putStrLn fname
+          else return ()
+    dir_walk "." worker
--- Haskell/assigment3/tests.hs
+++ Haskell/assigment3/tests.hs
@@ -1,4 +1,4 @@
 import Test.HUnit
 import Assignment3
 
-test1 = TestCase (assertEqual "for (foo 3)," (simpleFind "simpleFind") "simpleFind")
+test1 = TestCase (assertEqual "for (foo 3)," "simpleFind" (simpleFind "simpleFind"))
