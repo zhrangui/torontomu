4b2a017f27e8d90ab7f0fefb10f88ed7ac42ec99
Zhang <t902210@bell.corp.bce.ca>
2019-07-09 18:56:08 -0400

    rectangle area

--- Rust/lab9/src/main.rs
+++ Rust/lab9/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle{width: 5.0, length: 3.0};
+    print!("Rectangle area: {}", rect.area());
+}
+
+struct Rectangle {
+    width: f32,
+    length: f32,
+}
+
+impl Rectangle {
+    fn area(&self) -> f32 {
+        self.width * self.length
+    }
 }
