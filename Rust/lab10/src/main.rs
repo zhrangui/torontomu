cf409d484230a3b26de26851175e09163b4396d3
rui zhang <zhrangui@outlook.com>
2019-07-20 13:53:45 -0400

    lab10 strait

--- Rust/lab10/src/main.rs
+++ Rust/lab10/src/main.rs
@@ -1,3 +1,25 @@
+pub mod lab10 {
+    use core::f64::consts::PI;
+    pub struct Circle<T> {
+        pub r: T
+    }
+
+    pub trait Shape<T> {
+        fn area (&self) -> T;
+    }
+
+    impl Shape<f64> for Circle<f64> {
+        fn area(&self) -> f64 {
+            PI*self.r.powf(2.0)
+        }
+    }
+}
+use lab10::Shape;
+
 fn main() {
-    println!("Hello, world!");
+    let c = lab10::Circle{
+        r: 4.0
+    };
+    let a = c.area();
+    println!("Circle area: {}", a);
 }
