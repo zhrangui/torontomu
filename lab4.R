a0485f2f1de39d822fa16bc72ce0e77cacc9c504
Zhang <t902210@bell.corp.bce.ca>
2020-05-17 12:43:59 -0400

    assignment-1

--- lab4.R
+++ lab4.R
@@ -1,2 +1,54 @@
-lab4data <- read.csv("lab4dataset.csv", header=FALSE, stringsAsFactors=FALSE,na.strings=c("", "NA"))
+lab4data <- read.csv("lab4dataset.csv", stringsAsFactors=FALSE, sep=",", na.strings=c("", "NA"))
+str(lab4data)
+mean <- function(dt) {
+  sum(dt) / length(dt)
+}
 
+mean(lab4data$price)
+
+z.score <- function (dt, x) {
+  s = sd(dt)
+  (x - mean(dt)) /s
+}
+
+z.score(lab4data$age, 89)
+
+lower <- function(price, limit) {
+  price[price <= limit] 
+  
+}
+
+l  <- lower(lab4data$price, 100000)
+length(l)/length(lab4data$price)
+
+skew <- function(df) {
+  if (median(df) < mean(df)) {
+    return(1)
+  } else if (median(df) == mean(df)) {
+    return(0)
+  } else {
+    return(-1)
+  }
+}
+average <- function(x)
+{
+  return(sum(x, na.rm = T)/length(na.omit(x)))
+}
+Skewness <- function(x)
+{
+  hist(x, freq = T, breaks = 50)
+  return(sign(average(x) - median(x)))
+}
+Skewness(lab4data$price)
+zscore <- function(x,y)
+{
+  return((x - average(y))/sd(y))
+}
+bath <- function(price)
+{
+  print(summary(lab4data$bathp[lab4data$price<=price]))
+  return(zscore(average(lab4data$bathp[lab4data$price<=price]),
+                lab4data$bathp))
+}
+#Example
+bath(49120)
---
+++ quiz.R
@@ -0,0 +1,45 @@
+y <- matrix(nrow=2, ncol=3, 1:6)
+y[,2]
+y[ncol=2]
+
+bugsTotal <- c(40, 35, 60, 45)
+bugsSeverity <- c("Low", "Medium", "High", "Critical" )
+TrackingFrame <- data.frame(bugsSeverity, bugsTotal)
+TrackingFrame[2, 2]
+bugs <- c(1:5)
+patches <- c(2, 17, 8, 11, 5)
+Issues <- data.frame(Bugs=bugs, Patches =patches)
+
+Issues$Patches[ 1 : 5 ]
+x <- c(1, 2, NA, 4, NULL, 5)
+is.na(x)
+x <- c(1, 2, 3); y <- c(1, 2, 3, 4)
+z = x + y
+Y <- list(c(1,2,3), "Toronto")
+charCounter <- function(Str)
+{
+  nchar(Str)
+}
+charCounter("Hello, world!\n")
+
+
+f <- function(a)
+{
+  c(1:a)
+}
+f(4) - f(2)
+
+f <- function(a, b)
+{
+  x <- c(a:b)
+  y <- c(b:a)
+  x[a] - y[b]
+}
+f(1,4)
+
+f <- function(a)
+{
+  7
+  8
+}
+f(9)
