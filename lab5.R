464fed9a5f58217e2563cafce15fab911badc11c
rui zhang <zhrangui@outlook.com>
2020-05-19 21:24:15 -0400

    assignment-1 pdf

--- lab5.R
+++ lab5.R
@@ -1,2 +1,8 @@
-cars <- read.csv("auto-mpg.data", header=FALSE, stringsAsFactors=FALSE, sep="", na.strings=c("", "NA"))
+cars <- read.csv("auto-mpg.data", header=FALSE, stringsAsFactors=FALSE, sep="", na.strings=c("?","!", "NA"))
+summary(cars)
+str(cars)
+head(cars)
+cars$V4 <- as.numeric(cars$V4)
+plot(cars$V2,cars$V3)
+cars[which(is.na(cars)),]
 
