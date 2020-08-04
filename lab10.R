cabda19d8efce28de22451d4727034be1e52e693
rui zhang <zhrangui@outlook.com>
2020-08-04 18:22:18 -0400

    lab10

--- lab10.R
+++ lab10.R
@@ -4,3 +4,6 @@ install.packages('MASS')
 install.packages('leaps')
 library(MASS) # stepwise regression
 library(leaps) # all subsets regression
+library(RCurl) # getURL
+u <- getURL("http://vincentarelbundock.github.io/Rdatasets/csv/Ecdat/Computers.csv")
+c_prices <- read.csv("http://vincentarelbundock.github.io/Rdatasets/csv/Ecdat/Computers.csv")
