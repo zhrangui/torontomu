3f2268e3e255ae889d23fa09140fce36e5d98e3b
Rui Zhang <zhrangui@hotmail.com>
2023-07-03 23:04:12 -0400

    Lab 16: Egyptian Fractions

---
+++ EgyptianFractions.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+import java.util.List;
+
+public class EgyptianFractions {
+    public static List<BigInteger> greedy(Fraction f) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static List<BigInteger> splitting(Fraction a) {
+        throw new UnsupportedOperationException();
+    }
+}
