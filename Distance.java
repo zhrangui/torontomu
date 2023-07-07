47cfcfd58e4c1ecb2412a8a652513a439bbc729a
Rui Zhang <zhrangui@hotmail.com>
2023-07-07 09:53:46 -0400

    Lab 19: Symbolic Distances I: Representation

---
+++ Distance.java
@@ -0,0 +1,71 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Distance {
+    private TreeMap<Integer, Integer> coeff = new TreeMap<>();
+
+    public static int extractSquares(int n) {
+        int sqrt = (int) Math.sqrt(n);
+        int factor = 1;
+        for (int i = 2; i <= sqrt; i++) {
+            int sqr = i * i;
+            while (n % sqr == 0) {
+                factor *= i;
+                sqrt /= i;
+                n /= sqr;
+            }
+        }
+        return factor;
+    }
+
+    public Distance(int a, int b) {
+        Integer f = extractSquares(b);
+        coeff.put(b / (f * f), a * f);
+    }
+
+    public Distance(Map<Integer, Integer> coeff) {
+        for (Integer key : coeff.keySet()) {
+            int f = extractSquares(key);
+            int k = key / (f * f);
+            int coef = this.coeff.getOrDefault(k, 0);
+            this.coeff.put(k, coef + coeff.get(key) * f);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        for (Integer key : coeff.keySet()) {
+            if (coeff.get(key) == 0) {
+                continue;
+            }
+            if (sb.isEmpty()) {
+                if (key == 1) {
+                    sb.append(coeff.get(key));
+                    continue;
+                } else if ((coeff.get(key) < 0)) {
+                    sb.append("-");
+                }
+            } else {
+                if (coeff.get(key) > 0) {
+                    sb.append(" + ");
+                } else if ((coeff.get(key) < 0)) {
+                    sb.append(" - ");
+                }
+            }
+
+            if (coeff.get(key) != 1 && coeff.get(key) != -1) {
+                sb.append(Math.abs(coeff.get(key)));
+            }
+
+            if (key != 1) {
+                sb.append("Sqrt[" + key + "]");
+            }
+        }
+
+        if (sb.isEmpty()) {
+            sb.append("0");
+        }
+        return sb.toString();
+    }
+}
