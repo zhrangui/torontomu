00b9890d07ee1c74053fd3dc13771c4069d1b557
Rui Zhang <zhrangui@hotmail.com>
2023-07-08 21:27:23 -0400

    Lab 10: Computation Streams

---
+++ StreamExercises.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamExercises {
+    public static int countLines(Path path, int thres) throws IOException {
+        try (Stream<String> lines = Files.lines(path)) {
+            return (int) lines.filter(line -> line.length() >= thres).count();
+        }
+    }
+
+    public static List<String> collectWords(Path path) throws IOException {
+        try (Stream<String> lines = Files.lines(path)) {
+            return lines.flatMap(line -> Stream.of(line.toLowerCase()
+                    .split("[^a-z]+")))
+                    .filter(word -> !word.isEmpty())
+                    .sorted()
+                    .distinct()
+                    .collect(Collectors.toList());
+        }
+    }
+}
