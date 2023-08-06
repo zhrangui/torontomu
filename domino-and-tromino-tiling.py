45974cd472fb63a91cab36d47d2e6dc0f726f6f9
Rui Zhang <zhrangui@hotmail.com>
2023-08-05 22:31:47 -0400

    790. Domino and Tromino Tiling

---
+++ domino-and-tromino-tiling.py
@@ -0,0 +1,19 @@
+class Solution:
+    memo = {1:1,2:2,3:5}
+    def numTilings(self, n: int) -> int:
+        recur = self.memo.get(n)
+        if recur is not None:
+            return recur
+        recur_1 = self.numTilings(n-1)
+        recur_3 = self.numTilings(n-3)
+        recur = (recur_1*2 + recur_3)%(10**9 + 7)
+        self.memo[n] = recur
+        return recur
+
+def test_solution():
+    solution = Solution()
+    assert solution.numTilings(3) == 5
+
+if __name__ == '__main__':
+    import pytest
+    pytest.main([__file__])
