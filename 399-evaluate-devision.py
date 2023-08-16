42832ee4c9e9397e6bc28a598af3cb1ccdee5002
Rui Zhang <zhrangui@hotmail.com>
2023-08-16 15:29:59 -0400

    graph to traverse

--- 399-evaluate-devision.py
+++ 399-evaluate-devision.py
@@ -6,44 +6,36 @@ class Solution:
     399. Evaluate Division
     """
 
+    def find(self, start: str, end: str) -> float:
+        edges = self.graph.get(start, {})
+        if start in self.visited:
+            return
+        self.visited.add(start)
+        for vert, value in edges.items():
+            if vert == end:
+                return value
+            accum = self.find(vert, end)
+            if accum is not None:
+                return accum*value
+
+    def calcOne(self, query: List[str]) -> float:
+        self.visited = set()
+        sum = self.find(query[0], query[1])
+        return round(sum, 5) if sum else -1
+
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def calcOne(query: List[str]) -> float:
-            isExist = False
-            for i, eq in enumerate(equations):
-                if eq[0] in query or eq[1] in query:
-                    isExist = True
+        self.graph = {}
+        for i, eq in enumerate(equations):
+            edges = self.graph.get(eq[0], {})
+            weight = edges.get(eq[1], values[i])
+            edges.update([(eq[1], weight)])
+            self.graph.update([(eq[0], edges)])
+            edges = self.graph.get(eq[1], {})
+            weight = edges.get(eq[0], 1/values[i])
+            edges.update([(eq[0], weight)])
+            self.graph.update([(eq[1], edges)])
 
-                # if eq[1] == query[0] or eq[1] == query[1]:
-                #     isExist = True
-                #     assembly = [eq[1], eq[1]]
-                # if assembly:
-                for j, factor in enumerate(equations[i:]):
-                    if j == 0:
-                        assembly = eq[::]
-                        multiplication = values[i]
-                    else:
-                        if assembly[0] == factor[0]:
-                            assembly[0] = factor[1]
-                            multiplication /= values[i+j]
-                        elif assembly[0] == factor[1]:
-                            assembly[0] = factor[0]
-                            multiplication *= values[i+j]
-                        elif assembly[1] == factor[0]:
-                            assembly[1] = factor[1]
-                            multiplication *= values[i+j]
-                        elif assembly[1] == factor[1]:
-                            assembly[1] = factor[0]
-                            multiplication /= values[i+j]
-                    if assembly == query:
-                        return round(multiplication, 5)
-                    elif assembly[::-1] == query:
-                        return round(1.0/multiplication, 5)
-
-            if (query[0] == query[1]) and isExist:
-                return 1.0
-            return -1.0
-
-        result = list(map(calcOne, queries))
+        result = list(map(self.calcOne, queries))
         return result
 
 
@@ -51,13 +43,9 @@ def test_solution():
     solution = Solution()
     assert solution.calcEquation([["a", "b"], ["b", "c"]], [2.0, 3.0], [["a", "c"], ["b", "a"], [
                                  "a", "e"], ["a", "a"], ["x", "x"]]) == [6.00000, 0.50000, -1.00000, 1.00000, -1.00000]
-
     assert solution.calcEquation([["a", "b"], ["b", "c"], ["bc", "cd"]], [1.5, 2.5, 5.0], [
                                  ["a", "c"], ["c", "b"], ["bc", "cd"], ["cd", "bc"]]) == [3.75000, 0.40000, 5.00000, 0.20000]
-
-    assert solution.calcEquation([["a", "b"], ["e", "f"], ["b", "e"]], [3.4, 1.4, 2.3], [["b", "a"], ["a", "f"], ["f", "f"], [
-                                 "e", "e"], ["c", "c"], ["a", "c"], ["f", "e"]]) == [0.29412, 10.94800, 1.00000, 1.00000, -1.00000, -1.00000, 0.71429]
-
+    assert solution.calcEquation([["a","b"]], [0.5], [["a","b"],["b","a"],["a","c"],["x","y"]]) == [0.50000,2.00000,-1.00000,-1.00000]
 
 if __name__ == '__main__':
     import pytest
