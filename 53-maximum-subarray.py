794a82a0abdae9eda8b304bd70de538e701b62bb
Rui Zhang <zhrangui@hotmail.com>
2023-08-17 18:44:15 -0400

    53. Maximum Subarray

--- 53-maximum-subarray.py
+++ 53-maximum-subarray.py
@@ -1,18 +1,30 @@
 from typing import List
 
+
 class Solution:
     """
     53. Maximum Subarray
     """
 
     def maxSubArray(self, nums: List[int]) -> int:
-        ...
+        max = nums[0]
+        sum = 0
+        for i, n in enumerate(nums):
+            if sum < 0:
+                sum = n
+            else:
+                sum += n
+            if sum > max:
+                max = sum
+        return max
+
 
 def test_solution():
     solution = Solution()
-    assert solution.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == 6
+    assert solution.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
     assert solution.maxSubArray([1]) == 1
-    assert solution.maxSubArray([5,4,-1,7,8]) == 23
+    assert solution.maxSubArray([5, 4, -1, 7, 8]) == 23
+
 
 if __name__ == '__main__':
     import pytest
