d7a0bb70162e6c0e2ec5f1a3f885e342ee33d5de
Rui Zhang <zhrangui@gmail.com>
2023-09-24 11:49:33 -0400

    selection_sort

--- sort.cpp
+++ sort.cpp
@@ -8,24 +8,41 @@
 #include <iostream>
 #include <stdlib.h>
 
-// You are not limited to these functions. 
+// You are not limited to these functions.
 // Write as many helpers as you want
 // You might find it useful to overload and
 // pass additional arguments, for example.
 
 void selection_sort(int *arr, int n)
 {
-	// Your code here
+    int minIndex;
+    int min;
+    for (int i = 0; i < n; i++)
+    {
+        min = arr[i];
+        minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < min)
+            {
+                min = arr[j];
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            arr[minIndex] = arr[i];
+            arr[i] = min;
+        }
+    }
 }
 
 void merge_sort(int *arr, int n)
 {
-	// Your code here
+    // Your code here
 }
 
 void merge_sel_sort(int *arr, int n)
 {
     // Your code here
 }
-
-
