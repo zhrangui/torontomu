149f700c8377e8b50f7864b964a612a29a139592
Rui Zhang <zhrangui@gmail.com>
2023-09-25 21:29:34 -0400

    merge_sort

--- sort.cpp
+++ sort.cpp
@@ -37,12 +37,59 @@ void selection_sort(int *arr, int n)
     }
 }
 
+void merge(int *arr, int n, int m)
+{
+    int *copy_arr = new int[n];
+    std::copy(arr, arr + n, copy_arr);
+    int j = 0;
+    int k = m;
+    for (int i = 0; i < n; i++)
+    {
+        if (k >= n)
+        {
+            arr[i] = copy_arr[j];
+            j++;
+        }
+        else if (j >= m)
+        {
+            arr[i] = copy_arr[k];
+            k++;
+        }
+        else
+        {
+            if (copy_arr[j] < copy_arr[k])
+            {
+                arr[i] = copy_arr[j];
+                j++;
+            }
+            else
+            {
+                arr[i] = copy_arr[k];
+                k++;
+            }
+        }
+    }
+    delete[] copy_arr;
+}
+
 void merge_sort(int *arr, int n)
 {
-    // Your code here
+    if (n <= 1)
+        return;
+    int m = n / 2;
+    merge_sort(arr, m);
+    merge_sort(arr + m, n - m);
+    merge(arr, n, m);
 }
 
 void merge_sel_sort(int *arr, int n)
 {
-    // Your code here
+    if (n < 6)
+    {
+        selection_sort(arr, n);
+    }
+    else
+    {
+        merge_sort(arr, n);
+    }
 }
