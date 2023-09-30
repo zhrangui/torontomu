29c7e2d91541c12dab2d09e582df9064d51393fd
Rui Zhang <zhrangui@hotmail.com>
2023-09-30 09:08:24 -0400

    n+1

--- sort.cpp
+++ sort.cpp
@@ -16,7 +16,7 @@
 void selection_sort(int *arr, int n)
 {
     int minIndex;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n-1; i++)
     {
         minIndex = i;
         for (int j = i + 1; j < n; j++)
